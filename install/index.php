014f59572a89b3ffaaf6ce3f0c3ad5d2ec73d166
Михаил Кройтор <m_croitor@hotmail.com>
2018-10-18 17:57:35 +0300

    installation script updated

--- install/index.php
+++ install/index.php
@@ -8,6 +8,15 @@ include_once '../core/classes/db.class.php';
 
 $db = new db($dbcfg);
 
+// install config
+$q = "CREATE TABLE config_tbl ("
+        . "variable_id INT NOT NULL AUTO_INCREMENT, "
+        . "variable_name VARCHAR(255) NOT NULL,"
+        . "variable_value VARCHAR(255) NOT NULL,"
+        . "variable_type VARCHAR(255) NOT NULL,"
+        . "PRIMARY KEY(variable_id))";
+$db->do_query($q, false);
+
 // user module
 $q = "CREATE TABLE user_tbl ("
         . "user_id INT NOT NULL AUTO_INCREMENT, "
